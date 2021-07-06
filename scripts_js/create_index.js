ee6b2e5625df5648e786a35a2a55c76cba51d996
Isleimar <isleimar@gmail.com>
2021-07-06 19:17:42 -0300

    Construção dos scripts do documento 'Producao'

--- scripts_js/create_index.js
+++ scripts_js/create_index.js
@@ -1,3 +1,4 @@
 db.imdb_title_basics.createIndex({primary_title: 1})
 db.netflix_title.createIndex({title: 1})
 db.tmdb_titles.createIndex({original_title: 1})
+db.imdb_title_principals.createIndex({tconst: 1})
--- scripts_js/create_producao.js
+++ scripts_js/create_producao.js
@@ -1,6 +1,7 @@
-db.netflix_title.aggregate([
+db.netflix_imdb.aggregate([
     {$project:{
-        imdb_id: "$imdb_id",
+        _id: "$imdb_id",
+        netflix_id: "$_id",
         tipo: "$type",
         titulo: "title",
         pais: "$country",
@@ -10,8 +11,94 @@ db.netflix_title.aggregate([
         duracao: "$duration",
         genero: "$listed_in",
         sinopse: "$desciption"        
-    }},
-    {$out: {
-        db: "movies", coll: "producoes"
     }}
-])
+    //---lookup tmdb--------------------------------------
+    ,{$lookup: {
+        from: "tmdb_titles",
+        localField: "_id",
+        foreignField: "_id",
+        as: "tmdb"
+    }}
+    ,{$unwind: "$tmdb"}
+    ,{$addFields:{
+        idioma_original: "$tmdb.original_language",
+        popularidade: "$tmdb.popularity",
+        lista_idiomas: "$tmdb.spoken_languages",
+        total_votos: "$tmdb.vote_count",
+        avaliacao: "$tmdb.vote_average",
+        orcamento: "$tmdb.budget",
+        produtoras: "$tmdb.production_companies"
+
+    }}
+    ,{$unset: "tmdb"}    
+    //-----Produtora---------------------------------------
+    // ,{$unwind: {
+    //      path: "$produtoras",
+    //      preserveNullAndEmptyArrays: true
+    // }}
+    // ,{$addFields: {
+    //     produtoras :{ 
+    //         nome:  "$produtoras.name",
+    //         pais_origem: "$produtoras.origin_country"
+    //     }
+    // }}
+    // ,{$unset: [
+    //     "produtoras.name",
+    //     "produtoras.origin_country",
+    //     "produtoras.logo_path"
+    // ]}
+    // ,{ $group:{
+    //     _id: "$_id",        
+    //     produtoras: {$push: "$produtoras"}
+    // }}
+    //--------Ator---------------------------------------
+    ,{$lookup: {
+        from: "imdb_title_principals",
+        let:{
+            id: "$_id"
+        },
+        pipeline:[
+            {$match: {                
+                $expr:{
+                    $and: [
+                        {$eq:["$$id", "$tconst"]},                        
+                        {$in: ["$category", ["actor", "actress"]]}
+                    ]
+                }
+            }}            
+            ,{$project:{
+                id: "$nconst",  
+                _id: 0,
+                categoria: "$category",
+                papeis: "$characters"
+            }}
+        ],
+        as: "atores"
+    }}
+    //--------Direção---------------------------------------
+    ,{$lookup: {
+        from: "imdb_title_principals",
+        let:{
+            id: "$_id"
+        },
+        pipeline:[
+            {$match: {                
+                $expr:{
+                    $and: [
+                        {$eq:["$$id", "$tconst"]},
+                        {$not: {$in: ["$category", ["actor", "actress"]]}}
+                    ]
+                }
+            }}            
+            ,{$project:{
+                id: "$nconst",  
+                _id: 0,
+                categoria: "$category",
+                papeis: "$characters"
+            }}
+        ],
+        as: "direcao"
+    }}
+    ,{$out: {db: "movies", coll: "producoes"}}
+    
+]).pretty()
